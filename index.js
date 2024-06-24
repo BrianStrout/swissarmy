60b69f4ba07ab1bb0d0972c193e1f342132a9f52
Brian J. Strout <brianjstrout@gmail.com>
2024-06-24 10:42:37 -0400

    up and runnning with pop up for billing

--- index.js
+++ index.js
@@ -19,6 +19,33 @@ carrots.forEach((carrot) => {
   //   console.log(kiddos[1]);
 });
 
+window.onload = (e) => {
+  const now = new Date();
+  const day = now.getDay(); // returns a number representing the day of the week, starting with 0 for Sunday
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+
+  if (day === "1") {
+    alert(`Today is ${day} and the time is ${hours}:${minutes}.`);
+  } else {
+    document.getElementById("modal-1").classList.remove("hidden");
+  }
+
+  document.getElementById("yes").addEventListener("click", (e) => {
+    document.getElementById("modal-1").classList.add("hidden");
+  });
+  document.getElementById("no").addEventListener("click", (e) => {
+    window.open("https://app.invoicing.co/#/invoices", "_blank");
+
+    window.open(
+      "https://pcgus.my.workfront.com/team/64010c2a000400c0ea326b63c22878f5/schedule",
+      "_blank"
+    );
+  });
+
+  // document.getElementById("no").classList.remove("hidden");
+};
+
 // .onclick = function () {
 //   navigator.clipboard.writeText("Brianjstrout@gmail.com");
 // };
